816bc8ad99db3bf6bae8051bc7aa3da2236bad64
ChasePham <chase9659@gmail.com>
2023-06-12 16:57:35 -0500

    api

--- GoogleBardWatchOS Watch App/BardAPI.swift
+++ GoogleBardWatchOS Watch App/BardAPI.swift
@@ -5,48 +5,23 @@
 //  Created by Chase Pham on 6/12/23.
 //
 
+import OpenAISwift
 import Foundation
 
 
 class BardAPI{
-    private let apiKey: String
-    // This allows us to communicate and make requests with the api
-    private let shareSession: URLSession.shared
+    let shared = BardAPI()
+    let api_key = ""
     
-    private var headers:[String:String] {
-        ["Content-Type": "application/json",
-         "Authorization": "Bearer\(apiKey)"
-         ]
+    private init(){}
+    
+    
+    public func setup(){
+        let client = OpenAISwift(authToken: api_key)
     }
     
-    private let jsonDecoder = JSONDecoder()
-    
-    private var urlRequest: URLRequest{
-        let url = URL(string: "https://api.openai.com/v1/completions")!
-        var urlRequest = URLRequest(url:url)
-        urlRequest.httpMethod = "POST"
-        headers.forEach {urlRequest.setValue($1, forHTTPHeaderField: $0)}
-        return urlRequest
-    }
-    
-    
-    init(apiKey: String) {
-        self.apiKey = apiKey
-    }
-    
-    private func jsonBody(text:String, stream: Bool = true) throws -> Data{
-        let jsonBody:[String:Any] = [
-            "model": "text-chat-davinci-002-20230126",
-            "temperature":0.5,
-            "max_tokens":1024,
-            "prompt": text,
-            "stop":[
-                "\n\n\n",
-                "<|im_end|>"
-            ],
-            "stream":stream
-        ]
-        return try JSONSerialization.data(withJSONObject: jsonBody)
+    public func gettingResponse(text: String, completion: @escaping (Result<String,Error>) -> Void) {
+        
     }
     
 }
