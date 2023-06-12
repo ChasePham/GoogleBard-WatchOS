d6fc4dbd865e30533704ae1c635f94b2e3eaa59b
ChasePham <chase9659@gmail.com>
2023-06-12 12:46:35 -0500

    files

---
+++ GoogleBardWatchOS Watch App/BardAPI.swift
@@ -0,0 +1,8 @@
+//
+//  BardAPI.swift
+//  GoogleBardWatchOS Watch App
+//
+//  Created by Chase Pham on 6/12/23.
+//
+
+import Foundation
---
+++ GoogleBardWatchOS Watch App/GoogleBardWatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleBardWatchOSApp.swift
+//  GoogleBardWatchOS Watch App
+//
+//  Created by Chase Pham on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GoogleBardWatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
